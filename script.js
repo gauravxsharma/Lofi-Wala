d80676c3f85c064517a83ba2d2e99263a6934180
Gaurav Sharma <138442030+gauravxsharma@users.noreply.github.com>
2024-11-21 05:11:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const songItemsPlay = document.querySelectorAll(".songItemsPlay");
 const songs = [
     { title: "Let me Love You", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
     { title: "On My Way", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
-    { title: "Faded", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" }
-    { title: "Let me Love You", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
-    { title: "On My Way", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
-    { title: "Faded", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" }
+    { title: "Faded", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { title: "Alone", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { title: "Bella Ciao", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { title: "Maxico", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
     { title: "Let me Love You", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
     { title: "On My Way", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
-    { title: "Faded", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" }
-    { title: "Faded", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" }
+    { title: "Faded", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
+    { title: "Let me Love You", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" }
 ];
 
 let audioElement = new Audio(songs[0].filePath);
