86c4c67e45a339f277a9a8402f2b294ac047688f
Gaurav Sharma <138442030+gauravxsharma@users.noreply.github.com>
2024-11-21 05:05:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ const songs = [
     { title: "Let me Love You", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
     { title: "On My Way", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
     { title: "Faded", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" }
+    { title: "Let me Love You", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { title: "On My Way", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { title: "Faded", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" }
+    { title: "Let me Love You", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { title: "On My Way", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
+    { title: "Faded", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" }
+    { title: "Faded", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" }
 ];
 
 let audioElement = new Audio(songs[0].filePath);
